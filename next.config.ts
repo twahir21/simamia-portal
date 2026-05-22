d507f69185395e54d5e12f4b91dace42a6ac6feb
twahir21 <twahirsudy3@gmail.com>
2026-05-22 08:26:41 +0300

    redirect /apk to /download

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/apk',
+        destination: '/download',
+        permanent: true, // 308 permanent redirect (use false for 307 temporary)
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
